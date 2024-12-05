e4fbd9966069a2f6f5c006930883d6f112679197
DonatasNoreika <donoras@gmail.com>
2024-12-05 19:54:01 +0200

    Pridėtaws skaiciavimai.html

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def user():
     return render_template("index.html")
 
+@app.route("/skaiciavimai")
+def skaiciavimai():
+    return render_template("skaiciavimai.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
