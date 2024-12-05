4bde352669b2ae7c736d504b1872a5db98c11878
DonatasNoreika <donoras@gmail.com>
2024-12-05 20:17:11 +0200

    Pridėjau zmones.html

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ def user():
 def skaiciavimai():
     return render_template("skaiciavimai.html")
 
+@app.route("/zmones")
+def zmones():
+    vardai = ['Jonas', 'Antanas', 'Petras']
+    return render_template("zmones.html", sarasas=vardai)
+
 if __name__ == "__main__":
     app.run(debug=True)
